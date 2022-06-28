360c8f603519e471751c727b2b5dd5cbda3b0084
Mauro Nievas <54127211+Mauro-jpg@users.noreply.github.com>
2022-06-27 22:17:15 -0300

    template

---
+++ addax-react-landing-page-template/addax-react-light/addax/mailer.js
@@ -0,0 +1,38 @@
+const nodemailer = require('nodemailer')
+const sgTransport = require('nodemailer-sendgrid-transport')
+
+const transporter = nodemailer.createTransport(sgTransport({
+    auth: {
+        // Update sendgrid API here
+        api_key: '###'
+    }
+}))
+
+const send = ({ name, email, phone, text }) => {
+
+    const textBody = `Name: ${name}   
+                Email: ${email}
+                Phone: ${phone}
+                Body: ${text}
+                This email came from Addax React.js & Express.js template
+                `
+
+    const from = name && email ? `${name} <${email}>` : `${name || email}`
+
+    const message = {
+        from,
+        // Update the email here
+        to: 'exampleyour@gmail.com',
+        subject: 'New Email From Addax',
+        text: textBody,
+        replyTo: from
+    }
+
+    return new Promise((resolve, reject) => {
+        transporter.sendMail(message, (error, info) =>
+            error ? reject(error) : resolve(info)
+        )
+    })
+}
+
+module.exports = send
