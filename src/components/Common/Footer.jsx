ad12f2061602f9e4c5c7e7ea0bc76ac587b4bff9
Mauro Nievas <niev.mauro@gmail.com>
2022-07-03 20:11:41 -0300

    import refactor

--- src/components/Common/Footer.jsx
+++ src/components/Common/Footer.jsx
@@ -10,11 +10,7 @@ class Footer extends React.Component {
                         <div className="col-lg-12 col-md-12">
                             <h4><Link to="/"><span>a</span>ddax</Link></h4>
                             <ul>
-                                <li><Link to="https://www.facebook.com/" className="fa fa-facebook" target="_blank"></Link></li>
-                                <li><Link to="https://twitter.com/" className="fa fa-twitter" target="_blank"></Link></li>
-                                <li><Link to="#" className="fa fa-skype" target="_blank"></Link></li>
-                                <li><Link to="https://www.instagram.com/" className="fa fa-instagram" target="_blank"></Link></li>
-                                <li><Link to="https://www.linkedin.com/" className="fa fa-linkedin" target="_blank"></Link></li>
+                                <li><Link to="https://www.linkedin.com/company/novatech-uruguay/" className="fa fa-linkedin" target="_blank"></Link></li>
                             </ul>
                             <p>Copyright <i className="fa fa-copyright"></i> 2022 All Rights Reserved.</p>
                         </div>
