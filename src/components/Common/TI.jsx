672b26d2567e2f0ae20445f2c748517f59b6ac0c
MauroJs <niev.mauro@gmail.com>
2022-08-22 18:18:58 -0300

    cambio de  imagen

--- src/components/Common/TI.jsx
+++ src/components/Common/TI.jsx
@@ -7,10 +7,8 @@ class IT extends React.Component {
                 <section className="strategy-area">
                     <div className="container-fluid p-0">
                         <div className="row ">
-                            <div className="col-lg-6 col-md-5 col50">
-                                <video autoPlay muted loop className='colorNaranja'>         
-                                    <source src="/it.mp4" type="video/mp4"/>       
-                                </video>
+                            <div className="col50 col-md-6 TI">
+                                <div className="image noSizeMobile noBagroundPositionMobile"></div>
                             </div>
                             
                             <div className="col-lg-6 col-md-7 noSizeWork">
@@ -18,18 +16,18 @@ class IT extends React.Component {
                                     <div className="section-title">
                                         <h2><span>Soporte T.I.</span></h2>
                                         <p>Concéntrate en el negocio, nosotros en la tecnología
-<br></br>
-<br></br>
-Nos encargamos de administrar la infraestructura tecnológica de la organización.
-<br></br>
-<br></br>
-Ponemos a disposición personal técnico capacitado y comprometido para que todo funcione correctamente, creando un departamento de informática independiente de la estructura o tamaño.
-<br></br>
-<br></br>
-Ofrecemos asesoría tecnológica para asegurar que la empresa este actualizada y aplicando las mejores herramientas y soluciones para el negocio.
-<br></br>
-<br></br>
-Contamos con amplia experiencia en Administración de Centros de cómputos, Administración de Comunicaciones, Seguridad, Servidores, telefonía IP y Servicio a Usuarios.</p>
+                                        <br></br>
+                                        <br></br>
+                                        Nos encargamos de administrar la infraestructura tecnológica de la organización.
+                                        <br></br>
+                                        <br></br>
+                                        Ponemos a disposición personal técnico capacitado y comprometido para que todo funcione correctamente, creando un departamento de informática independiente de la estructura o tamaño.
+                                        <br></br>
+                                        <br></br>
+                                        Ofrecemos asesoría tecnológica para asegurar que la empresa este actualizada y aplicando las mejores herramientas y soluciones para el negocio.
+                                        <br></br>
+                                        <br></br>
+                                        Contamos con amplia experiencia en Administración de Centros de cómputos, Administración de Comunicaciones, Seguridad, Servidores, telefonía IP y Servicio a Usuarios.</p>
                                         <br></br>
                                         <p style={{color:"#298ACB"}}><b>Nos encargamos de administrar la infraestructura tecnológica de tu organización.</b></p>
                                     </div>
