d6eff23bf1b22aefbb6bc9ac5f433647cd6899b8
MauroJs <niev.mauro@gmail.com>
2022-08-22 17:56:58 -0300

    cambios varios

---
+++ client/build/precache-manifest.cec65854b2d00c729f5e0820d2088a14.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7d3fcc5cc7931b89f20",
+    "url": "/static/css/main.8985df2a.chunk.css"
+  },
+  {
+    "revision": "f7d3fcc5cc7931b89f20",
+    "url": "/static/js/main.dd84f6f1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "25394192bf8a5f0cacef",
+    "url": "/static/js/2.c8f1e642.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "7c49d1153abe2df2e96c6f0968be4052",
+    "url": "/static/media/ctr-bg.7c49d115.jpg"
+  },
+  {
+    "revision": "8042fa4830941b89c856d2f1474d38cd",
+    "url": "/static/media/faq-img.8042fa48.jpg"
+  },
+  {
+    "revision": "6ecdd7d69f2025ef7aff0bdbfc1f6a6a",
+    "url": "/static/media/dott.6ecdd7d6.jpg"
+  },
+  {
+    "revision": "a621840a945158354f4fe7799bf5ae8b",
+    "url": "/static/media/pattern.a621840a.png"
+  },
+  {
+    "revision": "7f3dc70f125f0feb3a99cadb399cb0fb",
+    "url": "/static/media/main-bg-2.7f3dc70f.jpg"
+  },
+  {
+    "revision": "d5f120980776935ca0e58d8c4d163cbd",
+    "url": "/static/media/funfact-bg.d5f12098.jpg"
+  },
+  {
+    "revision": "79044568c33a607142d1935037900fe3",
+    "url": "/static/media/nube.79044568.png"
+  },
+  {
+    "revision": "e73ab87d3e52edb7c55a5efc88f5da73",
+    "url": "/static/media/main-bg-3.e73ab87d.jpg"
+  },
+  {
+    "revision": "3b89f630ffa4aa7204a21b0abbd3c088",
+    "url": "/static/media/desarrollo.3b89f630.jpeg"
+  },
+  {
+    "revision": "1c84ecd60f4aa976c799f623f5208eb4",
+    "url": "/static/media/main-bg-1.1c84ecd6.jpg"
+  },
+  {
+    "revision": "d34e879ce64ba3ca7a762a5247d46d72",
+    "url": "/static/media/services-img5.d34e879c.jpg"
+  },
+  {
+    "revision": "86e59ced926d0718114c86da5f87842d",
+    "url": "/static/media/logo.86e59ced.png"
+  },
+  {
+    "revision": "0f9e83aeb5c1f4979bc77b43face3056",
+    "url": "/static/media/logo2.0f9e83ae.png"
+  },
+  {
+    "revision": "1203a4c7c4e0c953b97a8013dd1d0e3d",
+    "url": "/index.html"
+  }
+];
